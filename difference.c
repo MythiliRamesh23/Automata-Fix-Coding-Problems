7d0248630c53666871421488c5d8d04ae33ba2f9
Mythili Ramesh <66546893+MythiliRamesh23@users.noreply.github.com>
2021-05-20 10:59:39 +0530

    Problem 1
    Write a C Program for the following Scenario: Siva bought 17 Laptops altogether and his friend taken 8 laptops. How many laptops are left with Siva?

---
+++ difference.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1,num2,diff;
+    
+    scanf("%d %d",&num1,&num2);
+    diff=num1-num2;
+    printf("Remaining Laptops available with siva : %d",diff);
+    return  0;
+}
