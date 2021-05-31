b75ac07e067c2ed07a4773c4f455dd55e9d40695
Mythili Ramesh <66546893+MythiliRamesh23@users.noreply.github.com>
2021-05-31 12:18:47 +0530

    Create debug.c

---
+++ debug.c
@@ -0,0 +1,24 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main()
+{
+    int a=10,b;
+    
+
+    a>=5 ? b=100 : 200 ;
+
+    printf("%d" , b);
+
+    return 0;
+}
