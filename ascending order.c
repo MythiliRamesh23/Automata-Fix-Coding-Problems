9b3d31a1ebfb67611bdc2d3f32976f8f94eb7f29
Mythili Ramesh <66546893+MythiliRamesh23@users.noreply.github.com>
2021-05-31 12:00:16 +0530

    Create ascending order.c

---
+++ ascending order.c
@@ -0,0 +1,33 @@
+    #include <stdio.h>
+    int main()
+    {
+ 
+        int i, j, a, n, number[30];
+        scanf("%d", &n);
+        for (i = 0; i < n; ++i)
+            scanf("%d", &number[i]);
+ 
+        for (i = 0; i < n; ++i) 
+        {
+ 
+            for (j = i + 1; j < n; ++j)
+            {
+ 
+                if (number[i] > number[j]) 
+                {
+ 
+                    a =  number[i];
+                    number[i] = number[j];
+                    number[j] = a;
+ 
+                }
+ 
+            }
+ 
+        }
+ 
+        printf("The numbers arranged in ascending order are given below \n");
+        for (i = 0; i < n; ++i)
+            printf("%d\n", number[i]);
+ 
+    }
