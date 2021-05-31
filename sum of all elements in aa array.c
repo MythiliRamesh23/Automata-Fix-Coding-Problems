178eecf7b052714bf60e1e053245b6dd3f1ba8de
Mythili Ramesh <66546893+MythiliRamesh23@users.noreply.github.com>
2021-05-31 11:57:37 +0530

    Create sum of all elements in aa array.c

---
+++ sum of all elements in aa array.c
@@ -0,0 +1,22 @@
+ #include <stdio.h>
+ 
+ 
+int main()
+{
+    int a[1000],i,n,sum=0;
+    scanf("%d",&n);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+ 
+    
+    for(i=0; i<n; i++)
+    {
+         
+        sum+=a[i];
+    }
+     printf("%d",sum);
+ 
+    return 0;
+}
