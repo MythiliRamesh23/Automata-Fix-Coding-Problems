3c8ae1bc267078fa4748cc91dda7c92248a3e7b4
Mythili Ramesh <66546893+MythiliRamesh23@users.noreply.github.com>
2021-05-31 12:21:48 +0530

    Create error.c

---
+++ error.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+      int x;
+     scanf("%d",&x);
+
+     switch (x)
+
+     {
+
+     case 1: printf("welcome");
+             break;
+
+     case 2: printf("saveetha");
+
+     }
+
+    
+    return 0;
+}
