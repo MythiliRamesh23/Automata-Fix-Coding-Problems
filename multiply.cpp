f432fc518733eb4cd0419891aa7083dceecc7fac
Mythili Ramesh <66546893+MythiliRamesh23@users.noreply.github.com>
2021-05-31 12:10:10 +0530

    Create multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+   
+    float f1,f2,f3;
+
+    scanf("%f %f",&f1, &f2);
+
+    f3=f1*f2;
+    
+    scanf("%f",&f3);
+
+    printf("%.2f",f3);
+
+    return 0;
+
+}
