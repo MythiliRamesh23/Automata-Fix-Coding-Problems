65751231b750d08aefc25fd038f6628875cc4036
Mythili Ramesh <66546893+MythiliRamesh23@users.noreply.github.com>
2021-06-06 10:41:05 +0530

    Create arithmetic operations.cpp

---
+++ arithmetic operations.cpp
@@ -0,0 +1,23 @@
+ #include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+    int Num1, Num2;
+    
+    cin >> Num1 >> Num2;
+    
+    cout << "addition:" << Num1 + Num2 << endl;
+
+    cout << "subtraction:" << Num1 - Num2 << endl;
+
+    cout << "Multiplication:" << Num1 * Num2 << endl;
+    
+    cout << "division:" << Num1 / Num2 << endl;
+    
+    return 0;
+
+}
